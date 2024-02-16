82ad1e8db965f161ed3e0a8ff01c589fdad62667
Ashutosh7796 <132451174+Ashutosh7796@users.noreply.github.com>
2024-02-16 14:36:12 +0530

    OK

--- JWT-with-Spring-Security/src/main/java/com/spring/jwt/controller/AdminController.java
+++ JWT-with-Spring-Security/src/main/java/com/spring/jwt/controller/AdminController.java
@@ -11,8 +11,12 @@ import java.security.Principal;
 @RequestMapping("/admin")
 public class AdminController {
 
+
+
     @GetMapping("/index")
     public ResponseEntity<String> index(Principal principal){
         return ResponseEntity.ok("Welcome to admin page : " + principal.getName());
+
+
     }
 }
