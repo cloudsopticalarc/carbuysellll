4c67b7bf0ec2d318ec6e142db27e97567478c778
geeteshkumbalkar28 <geeteshkumbalkar@gmail.com>
2024-05-22 19:24:20 +0530

    ok

--- src/main/java/com/spring/jwt/config/AppConfig.java
+++ src/main/java/com/spring/jwt/config/AppConfig.java
@@ -147,7 +147,7 @@ public class AppConfig {
             @Override
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                 CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+                config.setAllowedOrigins(Collections.singletonList("https://cartechindia.com/"));
                 config.setAllowedMethods(Collections.singletonList("*"));
                 config.setAllowCredentials(true);
                 config.setAllowedHeaders(Collections.singletonList("*"));
